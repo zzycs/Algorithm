687765b08f2ce74ddcc78044bc1193ae89e3ada2
Zzy <ziyangz5@student.unimelb.edu.au>
2019-09-26 19:35:02 +1000

    线性查找

--- src/Algorithm_Array.java
+++ src/Algorithm_Array.java
@@ -21,7 +21,7 @@ public class Algorithm_Array {
 	/**
 	 * 求最长递增子序列长度
 	 */
-	public static int getLengthOfLIS(int[] array) {
+	static int getLengthOfLIS(int[] array) {
 		temp = new int[array.length];
 		temp[0] = array[0];
 		int length = 1;
@@ -40,7 +40,7 @@ public class Algorithm_Array {
 	/**
 	 * 打印最长递增子序列
 	 */
-	public static void printLIS(int[] array) {
+	static void printLIS(int[] array) {
 		int[] sortedArray = new int[array.length];
 		for (int i = 0; i < array.length; i++)
 			sortedArray[i] = array[i];
@@ -54,7 +54,7 @@ public class Algorithm_Array {
 	/**
 	 * 求最长公共子序列长度
 	 */
-	public static void getLengthOfLCS(int[] a1, int[] a2) {
+	static void getLengthOfLCS(int[] a1, int[] a2) {
 		dp = new int[a1.length + 1][a2.length + 1];
 		for (int i = 1; i <= a1.length; i++) {
 			for (int j = 1; j <= a2.length; j++) {
@@ -72,7 +72,7 @@ public class Algorithm_Array {
 	/**
 	 * 打印最长公共子序列
 	 */
-	public static void printLCS(int[] a1, int[] a2, int i, int j) {
+	static void printLCS(int[] a1, int[] a2, int i, int j) {
 		if (i == 0 || j == 0) return;
 		if (dp[i][j] == dp[i - 1][j]) {
 			printLCS(a1, a2, i - 1, j);
@@ -89,7 +89,7 @@ public class Algorithm_Array {
 	/**
 	 * 冒泡排序
 	 */
-	public static int[] bubbleSort(int[] array) {
+	static int[] bubbleSort(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = array.length - 1; j > i; j--) {
 				if (array[j] < array[j - 1]) {
@@ -103,7 +103,7 @@ public class Algorithm_Array {
 	/**
 	 * 鸡尾酒排序
 	 */
-	public static int[] cocktailSort(int[] array) {
+	static int[] cocktailSort(int[] array) {
 		int i, left = 0, right = array.length - 1;
 		while (left < right) {
 			for (i = left; i < right; i++) {
@@ -125,7 +125,7 @@ public class Algorithm_Array {
 	/**
 	 * 侏儒排序
 	 */
-	public static int[] gnomeSort(int[] array) {
+	static int[] gnomeSort(int[] array) {
 		for (int i = 0; i < array.length;) {
 			if (i == 0 || array[i] >= array[i - 1]) {
 				i++;
@@ -140,7 +140,7 @@ public class Algorithm_Array {
 	/**
 	 * 选择排序
 	 */
-	public static int[] selectionSort(int[] array) {
+	static int[] selectionSort(int[] array) {
 		for (int i = 0; i < array.length - 1; i++) {
 			int min = i;
 			for (int j = i + 1; j < array.length; j++) {
@@ -156,11 +156,11 @@ public class Algorithm_Array {
 	/**
 	 * 臭皮匠排序
 	 */
-	public static int[] stoogeSort(int[] array) {
+	static int[] stoogeSort(int[] array) {
 		return stoogeSortRecursion(array, 0, array.length - 1);
 	}
 
-	public static int[] stoogeSortRecursion(int[] array, int low, int high) {
+	static int[] stoogeSortRecursion(int[] array, int low, int high) {
 		if (array[high] < array[low]) {
 			exchange(array, low, high);
 		}
@@ -176,7 +176,7 @@ public class Algorithm_Array {
 	/**
 	 * 插入排序
 	 */
-	public static int[] insertSort(int[] array) {
+	static int[] insertSort(int[] array) {
 		for (int i = 1; i < array.length; i++) {
 			for (int j = i; j > 0 && array[j] < array[j - 1]; j--) {
 				exchange(array, j, j - 1);
@@ -188,7 +188,7 @@ public class Algorithm_Array {
 	/**
 	 * 希尔排序
 	 */
-	public static int[] shellSort(int[] array) {
+	static int[] shellSort(int[] array) {
 		int h = 1;
 		while (h < array.length / 3)
 			h = 3 * h + 1;
@@ -206,7 +206,7 @@ public class Algorithm_Array {
 	/**
 	 * 归并排序（自顶向下）
 	 */
-	public static int[] mergeSort1(int[] array) {
+	static int[] mergeSort1(int[] array) {
 		temp = new int[array.length]; // 分配空间
 		mergeSortRecursion(array, 0, array.length - 1);
 		return array;
@@ -215,7 +215,7 @@ public class Algorithm_Array {
 	/**
 	 * 归并排序（递归）
 	 */
-	public static void mergeSortRecursion(int[] array, int low, int high) {
+	static void mergeSortRecursion(int[] array, int low, int high) {
 		if (low >= high) return;
 		int mid = (low + high) / 2;
 		mergeSortRecursion(array, low, mid);
@@ -226,7 +226,7 @@ public class Algorithm_Array {
 	/**
 	 * 归并排序（自底向上）
 	 */
-	public static int[] mergeSort2(int[] array) {
+	static int[] mergeSort2(int[] array) {
 		temp = new int[array.length]; // 分配空间
 		for (int size = 1; size < array.length; size += size) { // 每一次归并的数组大小，1、2、4、8、16...
 			for (int low = 0; low < array.length - size; low += size * 2) {
@@ -239,7 +239,7 @@ public class Algorithm_Array {
 	/**
 	 * 快速排序
 	 */
-	public static int[] quickSort(int[] array) {
+	static int[] quickSort(int[] array) {
 		quickSortRecursion(array, 0, array.length - 1);
 		return array;
 	}
@@ -247,7 +247,7 @@ public class Algorithm_Array {
 	/**
 	 * 快速排序（递归）
 	 */
-	public static void quickSortRecursion(int[] array, int low, int high) {
+	static void quickSortRecursion(int[] array, int low, int high) {
 		if (low >= high) return;
 		int mid = partition(array, low, high);
 		quickSortRecursion(array, low, mid - 1);
@@ -257,7 +257,7 @@ public class Algorithm_Array {
 	/**
 	 * 三向切分快速排序
 	 */
-	public static int[] threeWayQuickSort(int[] array) {
+	static int[] threeWayQuickSort(int[] array) {
 		threeWayQuickSortRecursion(array, 0, array.length - 1);
 		return array;
 	}
@@ -265,7 +265,7 @@ public class Algorithm_Array {
 	/**
 	 * 三向切分快速排序（递归）
 	 */
-	public static void threeWayQuickSortRecursion(int[] array, int low, int high) {
+	static void threeWayQuickSortRecursion(int[] array, int low, int high) {
 		if (low >= high) return;
 		int element = array[low]; // 切分元素
 		int index = low + 1, lt = low, gt = high; // 三指针索引
@@ -285,7 +285,7 @@ public class Algorithm_Array {
 	/**
 	 * 堆排序
 	 */
-	public static int[] heapSort(int[] array) {
+	static int[] heapSort(int[] array) {
 		heapConstructor(array);
 		int size = array.length;
 		while (size > 0) {
@@ -298,7 +298,7 @@ public class Algorithm_Array {
 	/**
 	 * 桶排序
 	 */
-	public static int[] bucketSort(int[] array) {
+	static int[] bucketSort(int[] array) {
 		int min = array[0], max = array[0];
 		for (int element : array) {
 			max = Math.max(element, max);
@@ -320,7 +320,7 @@ public class Algorithm_Array {
 	/**
 	 * 计数排序
 	 */
-	public static int[] countSort(int[] array) {
+	static int[] countSort(int[] array) {
 		int min = array[0], max = array[0];
 		for (int element : array) {
 			max = Math.max(element, max);
@@ -343,9 +343,19 @@ public class Algorithm_Array {
 	/******************** 数组查找 ********************/
 
 	/**
+	 * 线性查找
+	 */
+	static boolean linearSearch(int[] array, int target, int low, int high) {
+		for (int i = low; i < high; i++) {
+			if (array[i] == target) return true;
+		}
+		return false;
+	}
+
+	/**
 	 * 二分查找（无重复数组）
 	 */
-	public static int binarySearch(int[] array, int target, int low, int high) {
+	static int binarySearch(int[] array, int target, int low, int high) {
 		int mid;
 		while (low < high) {
 			mid = (low + high) / 2;
@@ -363,7 +373,7 @@ public class Algorithm_Array {
 	/**
 	 * 查找重复有序数组中k的插入位置
 	 */
-	public static int getInsertPosition(int[] array, int k, int low, int high) {
+	static int getInsertPosition(int[] array, int k, int low, int high) {
 		int mid;
 		while (low <= high) {
 			mid = (low + high) / 2;
@@ -381,7 +391,7 @@ public class Algorithm_Array {
 	/**
 	 * 查找重复有序数组中k出现的次数
 	 */
-	public static int getNumberOfK(int[] array, int k, int low, int high) {
+	static int getNumberOfK(int[] array, int k, int low, int high) {
 		int mid;
 		while (low < high) {
 			mid = (low + high) / 2;
@@ -414,7 +424,7 @@ public class Algorithm_Array {
 	/**
 	 * 交换数组中的两个元素
 	 */
-	public static void exchange(int[] array, int i, int j) {
+	static void exchange(int[] array, int i, int j) {
 		int temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
@@ -423,7 +433,7 @@ public class Algorithm_Array {
 	/**
 	 * 原地归并左右数组
 	 */
-	public static int[] merge(int[] array, int low, int mid, int high) {
+	static int[] merge(int[] array, int low, int mid, int high) {
 		for (int k = low; k <= high; k++)
 			temp[k] = array[k]; // 更新数组
 		int i = low, j = mid + 1; // 双指针索引
@@ -444,7 +454,7 @@ public class Algorithm_Array {
 	/**
 	 * 切分
 	 */
-	public static int partition(int[] array, int low, int high) {
+	static int partition(int[] array, int low, int high) {
 		int element = array[low]; // 切分元素
 		int i = low, j = high + 1; // 双指针索引
 		while (true) {
@@ -464,7 +474,7 @@ public class Algorithm_Array {
 	/**
 	 * 堆构造
 	 */
-	public static int[] heapConstructor(int[] array) {
+	static int[] heapConstructor(int[] array) {
 		for (int i = array.length / 2 - 1; i >= 0; i--) {
 			sink(array, i, array.length);
 		}
@@ -474,7 +484,7 @@ public class Algorithm_Array {
 	/**
 	 * 堆下沉
 	 */
-	public static void sink(int[] array, int i, int size) {
+	static void sink(int[] array, int i, int size) {
 		while (2 * (i + 1) <= size) {
 			int j = 2 * i + 1; // 子节点索引
 			if (j < size - 1 && array[j] < array[j + 1]) // 选出子节点中较大值
@@ -489,7 +499,7 @@ public class Algorithm_Array {
 	/**
 	 * 堆上浮
 	 */
-	public static void swim(int[] array, int i) {
+	static void swim(int[] array, int i) {
 		while (i > 0) {
 			int j = (i - 1) / 2; // 父节点索引
 			if (array[i] <= array[j]) break;
@@ -500,6 +510,6 @@ public class Algorithm_Array {
 
 	/******************** 辅助字段 ********************/
 
-	public static int[] temp;
-	public static int[][] dp;
+	static int[] temp;
+	static int[][] dp;
 }
